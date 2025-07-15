d3fffdd8064e0c3e5ad04df2a3a5bb56c7fde61b
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-07-15 13:13:58 +0530

    origin

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-    baseURL: "http://localhost:4567"
+    // baseURL: "http://localhost:4567"
+    baseURL: "https://mini-order-management-2.onrender.com"
 });
 
 
--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: "*",
+    origin: "https://mini-order-management.vercel.app/",
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
